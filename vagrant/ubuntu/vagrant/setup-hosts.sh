6575c3ff49a42c9d9d7246007e98daebeda4f2b4
jokandre <jorgeandre93@yahoo.com.br>
2022-08-11 19:39:04 +0800

    Update IP

--- vagrant/ubuntu/vagrant/setup-hosts.sh
+++ vagrant/ubuntu/vagrant/setup-hosts.sh
@@ -2,16 +2,22 @@
 set -e
 IFNAME=$1
 ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
-sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
 
 # remove ubuntu-bionic entry
-sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+sed -e '/^.*ubuntu-focal.*/d' -i /etc/hosts
+# sed -e '/^.*master-*/d' -i /etc/hosts
+# sed -e '/^.*worker-*/d' -i /etc/hosts
+# sed -e '/^.*lb*/d' -i /etc/hosts
+
+
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
 
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
-192.168.5.11  master-1
-192.168.5.12  master-2
-192.168.5.21  worker-1
-192.168.5.22  worker-2
-192.168.5.30  lb
+192.168.56.11  master-1
+192.168.56.12  master-2
+192.168.56.21  worker-1
+192.168.56.22  worker-2
+192.168.56.30  lb
 EOF
